38d1e36e91e55a2ecad97466c15d05b06b3bfc8c
Andres-Montoya-SV <andreww470000@gmail.com>
2023-09-06 22:39:00 -0600

    Guias evaluacion 2

---
+++ DIB-TEC/js.js
@@ -0,0 +1,15 @@
+// Tomando como base la estructura esencial de una pagina web en HTML 5. Señale y asocie cada una de las partes a la descripción que le corresponda.
+
+// Partes:
+// En esta parte se ubica el logo
+// Se muestra un menú o lista desplegable
+// Se muestra información tal como: Listas de artículos, descripción de productos, entradas de un blog, otros
+// Muestra una serie de enlaces a diferentes lugares dentro del sitio web.
+// Es un complemento de la cabecera
+
+// Descripciones:
+// Cabecera
+// Barra de navegación
+// Información Principal
+// Barra lateral
+// Barra institucional
